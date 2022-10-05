4cabd08f99d15c22a12e862af981899ea7830879
Lexxa77 <Lexxa77.77@mail.ru>
2022-10-05 14:26:29 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Basket vasyaBasket = new Basket(10000);
+        vasyaBasket.add("Молоко", 80, 1.5);
+        vasyaBasket.add("Хлеб", 40, 0.4);
+
+        Basket petyaBasket = new Basket(5000);
+        petyaBasket.add("Лопата", 2800, 2.5);
+        petyaBasket.add("Бочка", 2900, 18);
+
+        Basket mashaBasket = new Basket("Стол", 5000, 22);
+
+
+        vasyaBasket.print("Корзина Васи:");
+        petyaBasket.print("Корзина Пети:");
+        mashaBasket.print("Корзина Маши:");
+
+    }
+}
